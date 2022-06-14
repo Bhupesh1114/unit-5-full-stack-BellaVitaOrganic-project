c140985f4eaf036d59e0920052db09e8329bc0b0
Bhupesh kumawat <bhupeshkumawat100@gmail.com>
2022-06-14 15:18:26 +0530

    added store content and redux-thunk

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,23 @@
+import { legacy_createStore as createStore, compose, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducers = combineReducers({
+
+});
+
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+
+const store = createStore(rootReducers, enhancer);
+
+export default store;

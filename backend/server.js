90f7d08661d6eff621c42b6869bb47ff937630a7
Bhupesh kumawat <bhupeshkumawat100@gmail.com>
2022-06-14 13:34:40 +0530

    boiler plate code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
@@ -11,11 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+app.use(morgan("dev"));
+
 const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => app.listen(PORT, () => {
-        console.log(`Server started at port: ${PORT} `)
+        console.log(`Server started at port: ${PORT} , "Database Connected" `)
     }))
     .catch((error) => console.log("Unable to connect to server", error))
 

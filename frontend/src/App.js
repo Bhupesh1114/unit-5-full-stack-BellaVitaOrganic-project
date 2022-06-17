ad37d214177596279a9a7ba633e24af5c4092c33
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-06-17 21:36:39 +0530

    add cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 
-import './App.css';
+import { Cartpage } from "./payment/Cartpage";
+// import { Routes, Route } from 'react-router-dom'
+// import { Information } from "./payment/Information";
 
 function App() {
   return (
+    <>
     <div className="App">
-     <h1>Hello React</h1>
+     <Cartpage/>
+
+     {/* <Information/> */}
+    {/* <Routes>
+      <Route />
+    </Routes> */}
+
+
+
     </div>
+    </>
   );
 }
 

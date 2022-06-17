eafeef475e0d3fba00ffb2eeac766fefae6f1d42
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-06-17 21:37:55 +0530

    delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,10 @@
 
-import { Cartpage } from "./payment/Cartpage";
-// import { Routes, Route } from 'react-router-dom'
-// import { Information } from "./payment/Information";
-
 function App() {
   return (
     <>
     <div className="App">
-     <Cartpage/>
+  
 
-     {/* <Information/> */}
-    {/* <Routes>
-      <Route />
-    </Routes> */}
 
 
 
